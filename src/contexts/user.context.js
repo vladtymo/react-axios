c38ed210630f3029016dc494b331e33d56af3626
Vlad Tymoshchuk <59306514+vladtymo@users.noreply.github.com>
2022-05-06 19:31:19 +0300

    Create reducer for working with user state.

--- src/contexts/user.context.js
+++ src/contexts/user.context.js
@@ -1,16 +1,62 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
 
 export const UserContext = createContext({
     // username property 
-    username: '',           // getter
-    setUsername: () => null // setter
+    username: '',            // getter
+    setUsername: () => null, // setter
+    clearUsername: () => null
     // other data ...
 });
 
-export const UserProvider = ({ children }) => {
+const INITIAL_STATE = {
+    username: ''
+}
 
-    const [username, setUsername] = useState('');
-    const value = { username, setUsername };
+const USER_ACTION_TYPES = {
+    CLEAR_USERNAME: 'CLEAR_USERNAME',
+    SET_USERNAME: 'SET_USERNAME'
+};
+
+/*
+    reducer = (state, action) => {
+        return { new state ... }
+    }
+*/
+
+const userReducer = (state, action) => {
+    // type - type of the action
+    // payload - data to do the aciton
+    const { type, payload } = action;
+
+    switch (type) {
+        case USER_ACTION_TYPES.CLEAR_USERNAME:
+            return {
+                //email: state.email,
+                //setUsername: state.setUsername,
+                ...state,
+                username: ''
+            };
+        case USER_ACTION_TYPES.SET_USERNAME:
+            return { ...state, username: payload };
+        default:
+            throw new Error('Invalid action type');
+    }
+}
+
+export const UserProvider = ({ children }) => {
+    const [{ username }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+    // action realizations
+    const clearUsername = () => {
+        dispatch({ type: USER_ACTION_TYPES.CLEAR_USERNAME });
+    }
+    const setUsername = (username) => {
+        dispatch({ type: USER_ACTION_TYPES.SET_USERNAME, payload: username });
+    }
+
+    //const [username, setUsername] = useState('');
+
+    const value = { username, setUsername, clearUsername };
 
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
