64fbf4c1f7bd26dd9751927679b85c8bffe1acb1
vladtymo <tymo.vlad@gmail.com>
2022-05-16 19:19:11 +0300

    Add axios dependency

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
-import { UserProvider } from './contexts/user.context';
 
 function App() {
   return (
-    // <UserProvider>
     <div className="App">
       <Navbar />
       <Outlet />
 
       {/* <footer>Copyright 2022</footer> */}
     </div>
-    // </UserProvider>
   );
 }
 
